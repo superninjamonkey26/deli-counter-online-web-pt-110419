be5043cdf1488066bbf1681316d2a41090698ec0
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-13 04:05:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,6 @@ def take_a_number(array, string)
   puts "Welcome, Ada. You are number 1 in line."
   elsif array.size > 0
   array.push("Grace")
+  puts "Welcome, Grace. You are number 4 in line."
   end
 end
