391123eb83fc06003e04434f5d01fdfcecfde9a1
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-13 04:26:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,6 +15,8 @@ end
 
 def now_serving(array)
   if array.size == 0
-  puts "There is nobody waiting to be served!"
+   puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.size}."
   end
 end
