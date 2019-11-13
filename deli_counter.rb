8e7102c2414fc9f9945406224f16417de1faba33
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-13 03:16:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@ def line(array)
   if array.size == 0
     puts "The line is currently empty."
   else array.size > 0
-    puts "The line is currently: "
+    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
   end
 end
