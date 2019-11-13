3363e762ab387326f143384c843d3512693ed584
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-13 04:28:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(array)
   if array.size == 0
     puts "The line is currently empty."
   elsif array.size > 0
-    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+    puts "The line is currently:"
   else another_deli
     return "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey"
   end
